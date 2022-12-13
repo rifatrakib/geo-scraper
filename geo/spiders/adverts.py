9f6b19d488ce4e6dc11dab9463e1306d911460d3
rifatrakib <Rakib.1508@outlook.com>
2022-12-13 10:08:49 +0600

    scraper added for collecting geospatial adverts data

---
+++ geo/spiders/adverts.py
@@ -0,0 +1,37 @@
+import json
+
+import scrapy
+
+from geo import settings
+
+
+class AdvertsSpider(scrapy.Spider):
+    name = "adverts"
+
+    def start_requests(self):
+        payload = {"default": True, "cacheable": True}
+        yield scrapy.Request(
+            url=settings.ADVERTS_COUNTER,
+            method="POST",
+            body=json.dumps(payload),
+            callback=self.parse_count,
+        )
+
+    def parse_count(self, response):
+        response = response.json()
+        if response["success"]:
+            number_of_pages = response["count"] // 50 + 1
+
+            for i in range(1, number_of_pages + 1):
+                payload = {"page": i, "cacheable": True}
+                yield scrapy.Request(
+                    url=settings.ADVERTS_PAGES,
+                    method="POST",
+                    body=json.dumps(payload),
+                    callback=self.parse,
+                )
+                break
+
+    def parse(self, response):
+        yield response.json()
+        yield response.meta
