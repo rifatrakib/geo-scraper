84df98a271cf900b158a7787f4cc8c400d764d4b
rifatrakib <Rakib.1508@outlook.com>
2022-12-27 12:11:26 +0600

    initial state for scraping threshold api

---
+++ geo/spiders/thresholds.py
@@ -0,0 +1,23 @@
+import scrapy
+
+from geo import settings
+from geo.utils import extract_data_from_stored_records
+
+
+class ThresholdsSpider(scrapy.Spider):
+    name = "thresholds"
+
+    def start_requests(self):
+        connector = settings.THRESHOLDS_CONNECTOR
+        identifier = settings.THRESHOLDS_IDENTIFIER
+        base_url = settings.THRESHOLDS_PAGES
+        records = extract_data_from_stored_records(connector, identifier)
+        for record in records:
+            parameter = record[identifier]
+            yield scrapy.Request(
+                url=f"{base_url}/{parameter}",
+                callback=self.parse,
+            )
+
+    def parse(self, response):
+        print(response.json())
