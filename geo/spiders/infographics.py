5e41083f7a968c394199d5c7095899e4e855a8d1
rifatrakib <Rakib.1508@outlook.com>
2023-01-21 07:07:18 +0600

    include metadata to set scraped records apart

--- geo/spiders/infographics.py
+++ geo/spiders/infographics.py
@@ -29,34 +29,38 @@ class InfographicsSpider(scrapy.Spider):
             yield scrapy.Request(
                 url=f"{flat_info_url}?{flat_info_query_parameter}={value}",
                 callback=self.parse_flat_info,
+                cb_kwargs={"id": value},
             )
 
             # request for warning
             yield scrapy.Request(
                 url=f"{warning_url}?{warning_query_parameter}={short_value}",
                 callback=self.parse_warning,
+                cb_kwargs={"id": value},
             )
 
             # request for purpose
             yield scrapy.Request(
                 url=f"{purpose_url}?{purpose_query_parameter}={short_value}",
                 callback=self.parse_purpose,
+                cb_kwargs={"id": value},
             )
 
             # request for plan
             yield scrapy.Request(
                 url=f"{plan_url}?{plan_query_parameter}={short_value}",
                 callback=self.parse_plan,
+                cb_kwargs={"id": value},
             )
 
-    def parse_flat_info(self, response):
-        print(response.json())
+    def parse_flat_info(self, response, **kwargs):
+        print({**kwargs, "data": response.json()})
 
-    def parse_warning(self, response):
-        print(response.json())
+    def parse_warning(self, response, **kwargs):
+        print({**kwargs, "data": response.json()})
 
-    def parse_purpose(self, response):
-        print(response.json())
+    def parse_purpose(self, response, **kwargs):
+        print({**kwargs, "data": response.json()})
 
-    def parse_plan(self, response):
-        print(response.json())
+    def parse_plan(self, response, **kwargs):
+        print({**kwargs, "data": response.json()})
